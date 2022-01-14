c0e2fb8bb013523f9e4c733daf93d0e3289b0dc4
schoetlr <srlrs20020@gmail.com>
2022-01-13 23:13:41 -0500

    tests

---
+++ test/counter_api_test.exs
@@ -0,0 +1,15 @@
+defmodule CounterApiTest do
+  use ExUnit.Case
+
+   test "uses Counter through API" do
+    pid = Counter.start(0)
+    assert Counter.state(pid) == 0
+
+    Counter.tick(pid)
+    Counter.tick(pid)
+
+    assert Counter.state(pid) == 2 
+  end
+
+end
+
--- test/counter_test.exs
+++ test/counter_test.exs
@@ -2,7 +2,8 @@ defmodule CounterTest do
   use ExUnit.Case
   doctest Counter
 
-  test "greets the world" do
-    assert Counter.hello() == :world
+   test "increments a value by 1" do
+    assert Counter.Core.inc(1) == 2
   end
 end
+
